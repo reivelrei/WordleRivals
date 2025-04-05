037205eaf2fc9894f9ebb70fa86f0d014851b452
reivelrei <78728639+reivelrei@users.noreply.github.com>
2025-04-05 20:32:34 +0200

    fix: Remove unnecessary secret_key

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_socketio import SocketIO
 from .game_manager import GameManager
@@ -8,9 +6,7 @@ socketio = SocketIO()
 game_manager = GameManager()
 
 def create_app():
-    load_dotenv()
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
     from . import routes
     app.register_blueprint(routes.bp)
