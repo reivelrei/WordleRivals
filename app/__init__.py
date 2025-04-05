6993e2cafeb895ef700e0cea36dc6245e0ada146
reivelrei <78728639+reivelrei@users.noreply.github.com>
2025-04-05 20:51:16 +0200

    fix: Adding secret_key config again

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_socketio import SocketIO
 from .game_manager import GameManager
@@ -6,7 +8,9 @@ socketio = SocketIO()
 game_manager = GameManager()
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
     from . import routes
     app.register_blueprint(routes.bp)
